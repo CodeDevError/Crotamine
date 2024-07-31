c6e3948a288bc121aaff686b4cb29947190a8fba
CodeDevError <coreycook09@icloud.com>
2024-07-31 12:35:38 +0100

    Delete Server directory
    Signed-off-by: CodeDevError <coreycook09@icloud.com>

--- Server/Website/js/screen.js
+++
@@ -1,8 +0,0 @@
-// js/screen.js
-export const showMainContent = (loadingScreen, mainContent, currentScreen) => {
-    setTimeout(() => {
-        loadingScreen.style.display = 'none';
-        mainContent.style.display = 'flex';
-        document.querySelector(`[data-screen="${currentScreen}"]`).click();
-    }, 5000);
-};
--- Server/command_handlers/simple_commands.py
+++
@@ -1,9 +0,0 @@
-def handle_simple_commands(server, command):
-    server.clients[server.current_client_ip].send(command.encode())
-    if command == 'sendmessage':
-        text = input("Enter the text: ")
-        server.clients[server.current_client_ip].send(text.encode())
-        title = input("Enter the title: ")
-        server.clients[server.current_client_ip].send(title.encode())
-    result_output = server.clients[server.current_client_ip].recv(1024).decode()
-    return result_output
